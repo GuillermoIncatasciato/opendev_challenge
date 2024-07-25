e4607ab54118f47e90d163756ae1cc824ce59ddc
Guillermo I <incatasciato.g.l@gmail.com>
2024-07-25 10:43:35 -0300

    Add DegreeController and SubjectController

--- app/controllers.py
+++ app/controllers.py
@@ -3,16 +3,18 @@ from . import schemas
 from .services import get_service
 
 
-
-class StudentController:
-
+class Controller:
     def __init__(self):
         self.router = APIRouter()
         self.service = get_service()
+
+class StudentController(Controller):
+
+    def __init__(self):
+        super().__init__()
         self.router.add_api_route("/students/", self.read_students, methods=["GET"])
         self.router.add_api_route("/students/{student_id}", self.read_student, methods=["GET"])
         self.router.add_api_route("/students/", self.create_student, methods=["POST"], status_code=status.HTTP_201_CREATED)
-
 
     def read_students(self, skip: int = 0, limit: int = 100):
         students = self.service.get_students_info(skip=skip, limit=limit)
@@ -31,7 +33,6 @@ class StudentController:
             raise HTTPException(status_code=404, detail="Student not found")
         return student
 
-
     def create_student(self, student_info: schemas.StudentInfoBase):
         student_with_same_email = self.service.get_student_by_email(email=student_info.email)
         if student_with_same_email:
@@ -44,3 +45,34 @@ class StudentController:
         student = self.service.insert_student_info(student_info)
 
         return {"student_id": student.id}
+
+
+class DegreeController(Controller):
+
+    def __init__(self):
+        super().__init__()
+        self.router.add_api_route("/degrees/", self.read_degrees, methods=["GET"])
+        self.router.add_api_route("/degrees/{degree_id}/subjects", self.read_subjects_by_degree, methods=["GET"])
+
+    def read_degrees(self):
+        return self.service.get_degrees()
+
+    def read_subjects_by_degree(self, degree_id: int):
+        degree = self.service.get_degree_by_id(degree_id)
+        if degree is None:
+            raise HTTPException(status_code=404, detail="Degree not found")
+        return self.service.get_subjects_by_degree(degree_id)
+
+
+class SubjectController(Controller):
+    def __init__(self):
+        super().__init__()
+        self.router.add_api_route("/subjects/", self.read_subjects, methods=["GET"])
+
+    def read_subjects(self):
+        return self.service.get_subjects()
+
+
+
+    
+
--- app/main.py
+++ app/main.py
@@ -1,26 +1,12 @@
-from fastapi import Depends, FastAPI, HTTPException
-from . import schemas
-from .services import Service, get_service
-from .controllers import StudentController
-
-app = FastAPI()
+from fastapi import FastAPI
+from .controllers import StudentController, DegreeController, SubjectController
 
 
 student_controller = StudentController()
-app.include_router(student_controller.router)
+degree_controller = DegreeController()
+subject_controller = SubjectController()
 
-@app.get("/subjects/")
-def read_degrees(service: Service = Depends(get_service)):
-    return service.get_subjects()
-
-@app.get("/degrees/")
-def read_degrees(service: Service = Depends(get_service)):
-    return service.get_degrees()
-
-@app.get("/degrees/{degree_id}/subjects")
-def read_degrees(degree_id: int, service: Service = Depends(get_service)):
-    degree = service.get_degree_by_id(degree_id)
-    if degree is None:
-        raise HTTPException(status_code=404, detail="Degree not found")
-    return service.get_subjects_by_degree(degree_id)
-
+app = FastAPI()
+app.include_router(student_controller.router, tags=["student"])
+app.include_router(degree_controller.router, tags=["degree"])
+app.include_router(subject_controller.router, tags=["subject"])
